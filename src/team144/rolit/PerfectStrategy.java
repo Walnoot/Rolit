775f3d8613742ec10cd0b5d107f0a2e6a6aa5bf3
willemsiers <willemsiers@github.com>
2013-12-16 17:16:39 +0100

    Addedd strategyy stubss andd didd somee thingss

---
+++ src/team144/rolit/PerfectStrategy.java
@@ -0,0 +1,20 @@
+package team144.rolit;
+
+public class PerfectStrategy implements Strategy {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public String getPlayer() {
+		return null;
+	}
+
+	@Override
+	public void makeMove() {
+	}
+
+
+}
