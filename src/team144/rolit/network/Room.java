abfa16674ba0f2217a77967689c3ed5adea6e3fc
willemsiers <willemsiers@github.com>
2014-01-24 15:31:17 +0100

    Created Room Support and Different gametypes

---
+++ src/team144/rolit/network/Room.java
@@ -0,0 +1,78 @@
+package team144.rolit.network;
+
+import java.util.ArrayList;
+
+import team144.util.Util;
+
+public class Room {
+    
+    /**
+     * C - 1v1 (requires name)
+     * D - default (2 players multiplayer)
+     * H - 1 random humans
+     * I - 2 random humans
+     * J - 3 random humans
+     */
+    public String type;
+    public int roomSize;
+    public ArrayList<Connection> players;
+    
+    public static ArrayList<Room> rooms;
+    
+    private Room(Connection player, String[] params) {
+        type = parseType(params);
+        if(type.equals("C") || type.equals("D")|| type.equals("H")){
+            roomSize = 2;
+        }else if(type.equals("I")){
+            roomSize = 3;
+        }else{
+            roomSize = 4;
+        }
+        addPlayer(player);
+    }
+    
+    private static String parseType(String[] params) {
+        String type;
+        if (params[0].equals("C")) {
+            type = params[1];
+        } else {
+            type = params[0];
+        }
+        return type;
+    }
+    
+    /**
+     * Looks if player fits in any existing room
+     * If not, creates a new rooms and puts him in it
+     * @param player - Connection of the player that requested a new game
+     * @param params - flags (gametype/player)
+     */
+    public static void assignRoom(Connection player, String[] params) {
+        if (rooms == null) rooms = new ArrayList<Room>();
+        
+        String wantedType = parseType(params);
+        
+        for (Room r : rooms) {
+            if (r.type.equals(wantedType)) {
+                r.addPlayer(player);
+            }
+        }
+        
+        //no rooms exists yet so make one
+        rooms.add(new Room(player,params));
+    }
+    
+    private void addPlayer(Connection player) {
+        players.add(player);
+        if(players.size()==roomSize){
+            String[] names = new String[players.size()];
+            for (int i = 0; i<roomSize; i++) {
+                names[i]=players.get(i).getName();
+            }
+            System.out.println("Room starting: "+Util.concat(names));
+            
+            player.write("START", names);
+        }
+    }
+    
+}
